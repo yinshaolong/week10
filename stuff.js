d84143958f3ee1c42ec6d85896dadffbbc06d02f
John Yin <shaolong1995@gmail.com>
2023-03-08 12:53:00 -0800

    added console log if statements

---
+++ stuff.js
@@ -0,0 +1,11 @@
+let result = "equal", isEqual = false
+let num1 = 1, num2 = 2
+if (num1 === num2){
+    isEqual = true
+}
+if (isEqual) {
+    console.log("The two numbers are "+ result)
+}
+else if (!isEqual){
+    console.log("The two numbers are not " + result)
+}
