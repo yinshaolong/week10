e3f84881c1e72e552cb4034f1724c62802e8bb0c
John Yin <shaolong1995@gmail.com>
2023-03-08 13:18:42 -0800

    added loops

--- stuff.js
+++ stuff.js
@@ -1,5 +1,5 @@
 let result = "equal", isEqual = false
-let num1 = 1, num2 = 2
+const num1 = 1, num2 = 2
 if (num1 === num2){
     isEqual = true
 }
@@ -8,4 +8,12 @@ if (isEqual) {
 }
 else if (!isEqual){
     console.log("The two numbers are not " + result)
+}
+for (let i = 0; i <= 10; i++) {
+    console.log(Math.pow(2, i))
+}
+let num = 0
+while (num != 11){
+    console.log(Math.pow(2, num))
+    num++ 
 }
