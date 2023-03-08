966f8dce27606d66fe7fee6f01d35b279825e755
John Yin <shaolong1995@gmail.com>
2023-03-08 13:33:32 -0800

    user prompt and alert

--- stuff.js
+++ stuff.js
@@ -17,3 +17,16 @@ while (num != 11){
     console.log(Math.pow(2, num))
     num++ 
 }
+num = 0
+do{
+    console.log(2 ** num);
+    num++;
+}while (num != 11)
+
+// user = prompt("What is your name?")
+// alert("Hello " + user)
+let user;
+do{
+    user = prompt("what is your name?")
+}while(!isNaN(user))
+alert("Hello " + user)
